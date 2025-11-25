096c89d14a74897785cdf40cef613a57075a532c
richmailYT <73596193+richmailYT@users.noreply.github.com>
2025-11-25 06:31:53 -0500

    fix: its "text", not "content" lol

--- src/types.ts
+++ src/types.ts
@@ -127,7 +127,7 @@ export type ToolChoice =
     };
 
 export type VerboseContent =
-    { type: 'text'; content: string }
+    { type: 'text'; text: string }
     | { type: 'image_url'; image_url: { url: string } }
     | { type: "file", file: { filename: string, file_data: string } };
 
